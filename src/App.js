98fd90a6eb2651d97de41c212c5941b44561f5ec
Nidhi Chaudhari <chaudharinidhi181@gmail.com>
2024-06-01 21:56:58 +0530

    Established routing && built navbar && created files for student CRUD operations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import Home from './Home';
 import './App.css';
 import StudentsView from './components/student/StudentsView';
+import Navbar from './components/common/Navbar';
+import StudentProfile from './components/student/StudentProfile';
+import AddStudent from './components/student/AddStudent';
+import EditStudent from './components/student/EditStudent';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <StudentsView/>
-    </div>
+    <main className="container mt-5">
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={<Home />}></Route>
+					<Route
+						exact
+						path="/view-students"
+						element={<StudentsView />}></Route>
+					<Route
+						exact
+						path="/add-students"
+						element={<AddStudent />}></Route>
+					<Route
+						exact
+						path="/edit-student/:id"
+						element={<EditStudent />}></Route>
+					<Route
+						exact
+						path="/student-profile/:id"
+						element={<StudentProfile />}></Route>
+				</Routes>
+			</Router>
+		</main>
   );
 }
 
